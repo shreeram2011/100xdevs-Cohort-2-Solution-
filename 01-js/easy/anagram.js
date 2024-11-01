09cb982711f777b017f9d32b7bc533e30f6fb2e0
Shreeram Yadav <yadavshreeram147@gmail.com>
2024-11-01 16:30:35 +0530

    first easy problem anagram is done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
+  const lowerword1 = str1.toLowerCase();
+  const lowerword2 = str2.toLowerCase();
 
+  if(lowerword1.length !== lowerword2.length){
+    return false;
+  }
+
+  const letterCount = {};
+  for(let letter of lowerword1){
+    letterCount[letter] = (letterCount[letter] || 0) + 1;
+  }
+
+  for(let letter of lowerword2){
+    if(!letterCount[letter]){
+      return false;
+    }
+    letterCount[letter]--;
+  }
+
+  return true;
 }
 
+console.log(isAnagram("top","pot"));
 module.exports = isAnagram;
